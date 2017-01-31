39040b74308b181bd2a63827660c159a26f5e6b3
Pedro Duarte <contact@peduarte.com>
2017-01-31 15:21:52 +0000

    Update config

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -23,11 +23,11 @@ function _root_segment
 end
 
 function _path_segment
-	_segment cyan (prompt_pwd)
+	_segment magenta (prompt_pwd)
 end
 
 function _git_upstream_configured
-	git rev-parse --abbrev-ref @"{upstream}" > /dev/null 2>&1
+	git rev-parse --abbrev-ref @"{u}" > /dev/null 2>&1
 end
 
 function _git_behind_upstream
@@ -35,47 +35,35 @@ function _git_behind_upstream
 end
 
 function _git_ahead_of_upstream
-	# test (git rev-list --left-only --count HEAD...@"{u}" ^ /dev/null) -gt 0
-	test (command git log ^/dev/null | grep '^commit' | wc -l | tr -d " ") -gt 0
+	test (git rev-list --left-only --count HEAD...@"{u}" ^ /dev/null) -gt 0
 end
 
 function _git_segment
 	if test (_git_branch_name)
 		set -l git_branch (_git_branch_name)
-		_segment blue $git_branch
+		_segment white $git_branch
 
 		if test (_is_git_dirty)
-			_segment yellow "±"
+			_segment yellow "☉"
 		end
 	end
 
 	if _git_upstream_configured
-		set -l git_status (command git rev-list --left-right --count 'HEAD...@{upstream}' | sed "s/[[:blank:]]/ /" ^/dev/null)
-
-		# Resolve Git arrows by treating `git_status` as an array
-		set -l git_arrow_left (command echo $git_status | cut -c 1 ^/dev/null)
-		set -l git_arrow_right (command echo $git_status | cut -c 3 ^/dev/null)
-		set -l git_arrows ""
-
-		# If arrow is not "0", it means it's dirty
-		if test $git_arrow_left -ne "0"
-			set git_arrows "↑";
+		if _git_behind_upstream
+			_segment yellow "▽"
 		end
 
-		if test $git_arrow_right -ne "0"
-			set git_arrows $git_arrows"↓"
+		if _git_ahead_of_upstream
+			_segment yellow "△"
 		end
-
-		_segment yellow $git_arrows
 	end
 end
 
 function _prompt_segment
 	if test $last_status = 0
-		set_color green
+		set_color cyan
 	else
 		set_color red
-		echo -n $last_status
 	end
 
 	echo ""
